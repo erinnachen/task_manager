4858fd1220971c1902b5fdde09f22900fcca5f67
Erinna Chen <erinna.chen@gmail.com>
2016-02-02 20:59:29 -0700

    Add edit button and form for updating a task

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -1,6 +1,7 @@
 require 'models/task_manager'
 class TaskManagerApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
+  set :method_override, true
 
   get '/' do
     erb :dashboard
@@ -23,6 +24,11 @@ class TaskManagerApp < Sinatra::Base
   get '/tasks/:id' do |id|
     @task = task_manager.find(id.to_i)
     erb :show
+  end
+
+  get '/tasks/:id/edit' do |id|
+    @task = task_manager.find(id.to_i)
+    erb :edit
   end
 
   not_found do
